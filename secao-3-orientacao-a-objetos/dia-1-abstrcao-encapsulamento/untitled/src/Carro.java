002cdd552bfcc0b02d2f8c37b54cb60b23c3dbb8
Ludson Pereira <ludson_ps27@hotmail.com>
2024-01-03 14:57:04 -0300

    exercicios conteudos - completos

--- secao-3-orientacao-a-objetos/dia-1-abstrcao-encapsulamento/untitled/src/Carro.java
+++ secao-3-orientacao-a-objetos/dia-1-abstrcao-encapsulamento/untitled/src/Carro.java
@@ -1,17 +1,47 @@
 public class Carro {
 
-  String marca;
-  String modelo;
-  int ano;
+  private String marca;
+  private int ano;
+  private String modelo;
+  public static int contador = 0;
 
   public Carro(String marca, String modelo, int ano) {
     this.marca = marca;
     this.modelo = modelo;
     this.ano = ano;
+
+    contador++;
   }
 
-  public exibirInformacoes() {
-    System.out.println(
-        String.format("Marca: %s, Modelo: %s, Ano: %i", this.marca, this.modelo, this.ano));
+  public void exibirInformacoes() {
+    System.out.printf("Marca: %s, Modelo: %s, Ano: %d%n", this.marca, this.modelo, this.ano);
+  }
+
+  public static void quantidadeDeCarros() {
+    System.out.printf("Total de carros: %d", contador);
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
   }
 }
--- secao-3-orientacao-a-objetos/dia-1-abstrcao-encapsulamento/untitled/src/Main.java
+++ secao-3-orientacao-a-objetos/dia-1-abstrcao-encapsulamento/untitled/src/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
   public static void main(String[] args) {
     Carro carro1 = new Carro("Mazda", "RX7", 1996);
+    Carro carro2 = new Carro("Mazda", "RX7", 1996);
+    Carro carro3 = new Carro("Mazda", "RX7", 1996);
+    Carro carro4 = new Carro("Mazda", "RX7", 1996);
+    Carro carro5 = new Carro("Mazda", "RX7", 1996);
+    Carro carro6 = new Carro("Mazda", "RX7", 1996);
+    Carro carro7 = new Carro("Mazda", "RX7", 1996);
+    carro1.exibirInformacoes();
+    Carro.quantidadeDeCarros();
   }
 }
