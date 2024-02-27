8845c6fcd12e6d73dd4c039ccc7f230b1593886b
Ludson Pereira <ludson_ps27@hotmail.com>
2024-02-27 15:26:12 -0300

    feat: add CategoryRepository repositories

---
+++ secao-5-persistencia-1/dia-2-spring-data-jpa/gerention/src/main/java/com/ludson/gerention/models/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ludson.gerention.models.repositories;
+
+import com.ludson.gerention.models.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
